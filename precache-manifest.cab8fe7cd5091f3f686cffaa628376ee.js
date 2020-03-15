5d2237baf78fa194dcb660e1ac27eef79b5dd76b
nikhil bagal <nbagal02@gmail.com>
2020-03-15 14:57:07 +0530

    Updates

---
+++ precache-manifest.cab8fe7cd5091f3f686cffaa628376ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc589f3785b3df49162d8fcc3c1f8096",
+    "url": "/quote-app/index.html"
+  },
+  {
+    "revision": "8adee9e0db22623e97d2",
+    "url": "/quote-app/static/css/main.f7fc5d57.chunk.css"
+  },
+  {
+    "revision": "5c38b6c62aa4567ed92a",
+    "url": "/quote-app/static/js/2.15c85fad.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/quote-app/static/js/2.15c85fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8adee9e0db22623e97d2",
+    "url": "/quote-app/static/js/main.09b8b7e7.chunk.js"
+  },
+  {
+    "revision": "0a467fc3407259e54de5",
+    "url": "/quote-app/static/js/runtime-main.4b329f2c.js"
+  }
+]);
